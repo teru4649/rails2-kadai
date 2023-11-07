252148a8d42801477cb0e03d4aa7423dc4b28059
DESKTOP-CVQLEKE\hihok <askteru2@gmail.com>
2023-11-07 14:47:33 +0000

    finish 2-8

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,5 @@
 class Goal < ApplicationRecord
   belongs_to :user
+  has_many :todos, -> { order("done ASC, positio ASC").includes(:goal) }, dependent: :destroy
+  validates :title, presence: true
 end
