dda5ea837b04ce09ead43e39fe7856a98b5820af
DESKTOP-CVQLEKE\hihok <askteru2@gmail.com>
2023-11-08 02:23:21 +0000

    finish 2-15

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_07_140850) do
+ActiveRecord::Schema.define(version: 2023_11_08_021102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,23 @@ ActiveRecord::Schema.define(version: 2023_11_07_140850) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_goals_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tags_on_user_id"
+  end
+
+  create_table "todo_tags", force: :cascade do |t|
+    t.bigint "todo_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_todo_tags_on_tag_id"
+    t.index ["todo_id"], name: "index_todo_tags_on_todo_id"
   end
 
   create_table "todos", force: :cascade do |t|
@@ -46,5 +63,8 @@ ActiveRecord::Schema.define(version: 2023_11_07_140850) do
   end
 
   add_foreign_key "goals", "users"
+  add_foreign_key "tags", "users"
+  add_foreign_key "todo_tags", "tags"
+  add_foreign_key "todo_tags", "todos"
   add_foreign_key "todos", "goals"
 end
